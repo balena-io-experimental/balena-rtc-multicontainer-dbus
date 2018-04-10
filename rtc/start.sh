d1823ddf5b6469d605310ee49cbaef9fa58bb4d7
Gergely Imreh <imrehg@gmail.com>
2018-04-10 16:40:05 +0100

    Update to the DBUS call

--- rtc/start.sh
+++ rtc/start.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-## Do the main work, whatever it may be...
+# Trigger loading system time from RTC
+# https://www.freedesktop.org/wiki/Software/systemd/timedated/
+# (the boolean:true below triggers that)
+DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
+ dbus-send \
+  --system \
+  --print-reply \
+  --reply-timeout=2000 \
+  --type=method_call \
+  --dest=org.freedesktop.timedate1  \
+  /org/freedesktop/timedate1  \
+  org.freedesktop.timedate1.SetLocalRTC \
+  boolean:false boolean:true boolean:false
+
+# Do the main work, whatever it may be...
+# Or don't do anything, and do the work in other services
 while : ; do date; sleep "${INTERVAL:-15}"; done
